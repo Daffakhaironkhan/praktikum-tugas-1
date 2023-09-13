89771ca563bb77b5efab1f8025ed518b6dcfdd5c
Daffakhaironkhan <daffa.khan@mhs.unsoed.ac.id>
2023-09-13 09:55:04 +0700

    Tugas 1 Praktikum Pemrograman Mobile

--- main.dart
+++ main.dart
@@ -6,10 +6,14 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int tahunSekarang = DateTime.now().year;
+    int usia = tahunSekarang - tahunLahir;
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021097", "Daffa Khairon Khan", 2002);
+  mhs.perkenalan();
 }
